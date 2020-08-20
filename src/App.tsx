f38a38274e65da0f203d2ce395416eb540c8969e
piters13 <piotr.marganski@gmail.com>
2020-08-20 14:52:15 +0200

    Initialize new repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import About from './About';
+import Home from './Home';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<nav>
+				<ul>
+					<li><Link to="/">Home</Link></li>
+					<li><Link to="/about">About</Link></li>
+				</ul>
+			</nav>
+			<Switch>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
