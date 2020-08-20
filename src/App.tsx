8d60314bb3aea068e2390293e874ffd7929ecf4b
piters13 <piotr.marganski@gmail.com>
2020-08-20 15:24:37 +0200

    Add the Suspense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React, {Suspense, lazy} from 'react';
 
-import About from './About';
-import Home from './Home';
-import React from 'react';
+const About = lazy(() => import('./About'));
+const Home = lazy(() => import('./Home'));
 
 function App() {
 	return (
 		<Router>
-			<nav>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/about">About</Link></li>
-				</ul>
-			</nav>
-			<Switch>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
+			<Suspense fallback={<div>Loading...</div>}>
+				<nav>
+					<ul>
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/about">About</Link></li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Suspense>
 		</Router>
 	);
 }
