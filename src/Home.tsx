13b4f948899795b52aed4fdf6f2fd3882582f634
piters13 <piotr.marganski@gmail.com>
2020-08-21 13:02:37 +0200

    Add label to input to improve accessibility

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,59 +1,59 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 type WeatherAPIResponse = {
-    "coord": { "lon": number,"lat": number},
-    "weather": [
-        {
-        "id": number,
-        "main": string,
-        "description": string,
-        "icon": string
-        }
-    ],
-    "base": string,
-    "main": {
-        "temp": number,
-        "feels_like": number,
-        "temp_min": number,
-        "temp_max": number,
-        "pressure": number,
-        "humidity": number
-    },
-    "wind": {
-        "speed": number,
-        "deg": number
-    },
-    "clouds": {
-        "all": number
-    },
-    "dt": number,
-    "sys": {
-        "type": number,
-        "id": number,
-        "message": number,
-        "country": string,
-        "sunrise": number,
-        "sunset": number
-    },
-    "timezone": number,
-    "id": number,
-    "name": string,
-    "cod": number
-}
+	coord: { lon: number; lat: number };
+	weather: [
+		{
+			id: number;
+			main: string;
+			description: string;
+			icon: string;
+		}
+	];
+	base: string;
+	main: {
+		temp: number;
+		feels_like: number;
+		temp_min: number;
+		temp_max: number;
+		pressure: number;
+		humidity: number;
+	};
+	wind: {
+		speed: number;
+		deg: number;
+	};
+	clouds: {
+		all: number;
+	};
+	dt: number;
+	sys: {
+		type: number;
+		id: number;
+		message: number;
+		country: string;
+		sunrise: number;
+		sunset: number;
+	};
+	timezone: number;
+	id: number;
+	name: string;
+	cod: number;
+};
 
 const api = {
 	key: '5b6878cb6e4e855206685e98133becff',
-	url: 'https://api.openweathermap.org/data/2.5/'
+	url: 'https://api.openweathermap.org/data/2.5/',
 };
 
 const Home: React.FC = () => {
-    const dateBuild = () => {
+	const dateBuild = () => {
 		let date = String(new window.Date());
-		date = date.slice(3,15);
+		date = date.slice(3, 15);
 		return date;
-	}
+	};
 
-	const [query, setQuery] = useState("");
+	const [query, setQuery] = useState('');
 	const [weather, setWeather] = useState({} as WeatherAPIResponse);
 
 	const search = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -61,51 +61,48 @@ const Home: React.FC = () => {
 			fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
 				.then(res => res.json())
 				.then(result => {
-					setQuery("");
+					setQuery('');
 					setWeather(result);
-                    console.log(result);
-				})
+					console.log(result);
+				});
 		}
 	};
 
-    return (
-        <div className={
-            typeof weather.main != 'undefined'
-                ? weather.main.temp > 18
-                    ? 'App hot'
-                    : 'App cold'
-                : 'App'
-        }>
-          <main>
-            <div className='search-container'>
-                <input type='text'
-                       placeholder='Search...'
-                       className='search-bar'
-                       onChange={(e) => setQuery(e.target.value)}
-                       value={query}
-                       onKeyPress={search} />
-            </div>
-            {typeof weather.main != 'undefined' 
-            ? (
-                <div>
-                    <div className="location-container">
-                        <div className="location">
-                            {weather.name}, {weather.sys.country}
-                        </div>
-                        <div className="date">{dateBuild()}</div>
-                    </div>
-                    <div className="weather-container">
-                        <div className="temperature">
-                            {Math.round(weather.main.temp)}°C
-                        </div>
-                        <div className="weather">{weather.weather[0].main}</div>
-                    </div>
-                </div>
-              ) 
-            : ("")}
-          </main>  
-        </div>
-    )
+	return (
+		<div className={typeof weather.main != 'undefined' ? (weather.main.temp > 18 ? 'App hot' : 'App cold') : 'App'}>
+			<main>
+				<div className='search-container'>
+					<label>
+						Enter city name:
+						<input
+							type='text'
+							placeholder='Search...'
+							className='search-bar'
+							onChange={e => setQuery(e.target.value)}
+							value={query}
+							onKeyPress={search}
+						/>
+					</label>
+				</div>
+				{typeof weather.main != 'undefined' ? (
+					<div>
+						<div className='location-container'>
+							<div className='location'>
+								{weather.name}, {weather.sys.country}
+							</div>
+							<div className='date'>{dateBuild()}</div>
+						</div>
+						<div className='weather-container'>
+							<div className='temperature'>{Math.round(weather.main.temp)}°C</div>
+							<div className='weather'>{weather.weather[0].main}</div>
+						</div>
+					</div>
+				) : (
+					''
+				)}
+			</main>
+		</div>
+	);
 };
 
 export default Home;
